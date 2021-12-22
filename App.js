d7e5e71d0ec44d67c765894fe4881363d69f2a0c
Ldiablo90 <ldiablo90@gmail.com>
2021-12-22 18:47:13 +0900

    fist commit
    expo 설치
    네이티브컨테이너
    스텍
    파이어베이스

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native'
+import firebase from 'firebase/compat/app';
+import { getAuth, onAuthStateChanged} from 'firebase/auth'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import LandingScreen from "./components/auth/Landing"
+import RagisterScreen from "./components/auth/Register"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1MyUVpZat8OlUm4ul3UEVEvPOHqgWjNE",
+  authDomain: "instagram-dev-react-64c9d.firebaseapp.com",
+  projectId: "instagram-dev-react-64c9d",
+  storageBucket: "instagram-dev-react-64c9d.appspot.com",
+  messagingSenderId: "814968229243",
+  appId: "1:814968229243:web:bb6f77e622e93825f6c1de",
+  measurementId: "G-GW14GX3NNT"
+};
+
+if(firebase.apps.length === 0 ){
+  firebase.initializeApp(firebaseConfig)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loaded: false
+    }
+  }
+  componentDidMount(){
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true
+        })
+      }else{
+        this.setState({
+          loggedIn: true,
+          loaded: true
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if(!loaded){
+      return(
+        <View style={{ flex:1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }else if(!loggedIn){
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Lading">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown:false }}/>
+            <Stack.Screen name="Register" component={RagisterScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      )
+    }
+    return(
+      <View style={{ flex:1, justifyContent: 'center' }}>
+        <Text>User is logged in</Text>
+      </View>
+    )
+  }
+}
+
+export default App
